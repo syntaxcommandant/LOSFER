33090cdf26415fcce38ee613061b3d20566af787
syntaxcommandant <prachikumari9508@gmail.com>
2026-09-13 00:14:07 +0530

    Removed tempCodeRunnerFile and updated gitignore

--- tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-
-# POST endpoint - claim reject karne ke liye
-@app.post("/claims/{claim_id}/reject")
-def reject_claim(claim_id: int):
-    for claim in claims_db:
-        if claim["id"] == claim_id:
-            claim["status"] = "rejected"
-            return {"message": "Claim rejected", "claim": claim}
-    raise HTTPException(status_code=404, detail="Claim not found")
